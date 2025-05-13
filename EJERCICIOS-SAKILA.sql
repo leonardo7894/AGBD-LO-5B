ec10fe1cb451e75b3726437ca654c879199b681f
leonardo ojeda <nahuelojeda2007@gmail.com>
2025-05-13 17:01:26 -0300

    hiciu actividades de chinook

--- EJERCICIOS-SAKILA.sql
+++ EJERCICIOS-SAKILA.sql
@@ -96,23 +96,95 @@ INNER JOIN category c on c.category_id = fc.category_id
 ORDER by length DESC
 
 14º
-SELECT * from film
+SELECT count(a.actor_id) as can_actores,a.first_name,f.title from film f
+INNER JOIN film_actor fa on f.film_id = fa.film_id
+INNER JOIN actor a on a.actor_id = fa.actor_id
 WHERE title like 'w%'
+GROUP by f.film_id
+HAVING can_actores >= 5
+ORDER by can_actores ASC
 
+15º 
+SELECT sum(amount) as total_pagos,first_name,last_name from payment p
+INNER JOIN customer c on c.customer_id = p.customer_id
+GROUP by c.customer_id
 
+16º
+SELECT title,length,first_name from film f
+INNER JOIN film_actor fa on fa.film_id = f.film_id
+INNER JOIN actor a on a.actor_id = fa.actor_id
+GROUP by first_name 
+ORDER by length ASC
 
+17º
+SELECT last_name,city,country,address,amount FROM customer cu
+INNER JOIN address a on a.address_id = cu.address_id
+INNER JOIN city c on c.city_id = a.city_id 
+inner JOIN country co on co.country_id = c.country_id
+INNER JOIN payment p on cu.customer_id = p.customer_id
+INNER JOIN rental r on cu.customer_id = r.customer_id
+GROUP by last_name
+order by amount ASC
 
-
+18º
 INSERT INTO actor(actor_id,first_name,last_name,last_update)
 VALUES("201","leonardo","ojeda", "2000-20-20")
 
+19º
+INSERT INTO actor(actor_id,first_name,last_name,last_update)
+VALUES("203","leonardo","ojeda", "2000-20-20"),
+      ("202","brigit","aguirre", "2000-21-25"),
+      ("204","ana","porco", "2025-21-99")
 
-
+20º
+UPDATE actor 
+SET first_name = "LALALAAL", last_name = "JJAJAJA"
+WHERE actor_id = 201;
 
 UPDATE actor
-SET first_name = "leo", last_name = "ojed"
-WHERE first_name = "leonardo" AND last_name = "ojeda"
+SET first_name = "LEONARDO", last_name = "OJEDA"
+WHERE actor_id = 202;
 
+UPDATE actor
+SET first_name = "BRIGIT", last_name = "AGUIRRE"
+WHERE actor_id = 203;
 
+UPDATE actor 
+SET first_name = "ANA", last_name = "PORCO"
+WHERE actor_id = 204;
+
+21º
 DELETE from actor
-WHERE actor_id = "201"
+WHERE actor_id = "201";
+
+DELETE FROM actor
+WHERE actor_id = "202";
+
+DELETE FROM actor
+WHERE actor_id = "203";
+
+DELETE FROM actor
+WHERE actor_id = "204";
+
+
+
+SENTENCIA CHINOOK
+
+1º
+SELECT FirstName,LastName FROM employees
+ORDER BY FirstName ASC
+
+2º
+SELECT  t.name, t.Milliseconds FROM tracks t
+INNER JOIN albums a on t.AlbumId = a.AlbumId
+WHERE a.Title like "big ones"
+ORDER by t.Milliseconds DESC
+
+3º
+SELECT count(t.name) as cant_music,g.name FROM tracks t
+INNER JOIN genres g on g.GenreId = t.GenreId
+GROUP by g.GenreId 
+
+4º
+
+
